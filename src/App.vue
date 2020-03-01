ee590e5de0dd21685530f734e46a1d0e7b3cf908
yujingsong1206 <646783598@qq.com>
2020-03-01 12:12:37 +0800

    封装storage

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-
+//import storage from './storage'
 export default {
   name: 'App',
   components: {
@@ -17,7 +17,9 @@ export default {
     }
   },
   mounted(){
-    
+    //storage.setItem('a',1);
+    //storage.setItem('user',{a:1});
+    //storage.setItem('abc',{a:1},'user');
   }
 }
 </script>
