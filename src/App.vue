ef0c40e1649ecb6eeb088846918cf7905f906efe
yujingsong1206 <646783598@qq.com>
2020-03-01 17:55:59 +0800

    mock设置

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-//import storage from './storage'
+
 export default {
   name: 'App',
   components: {
@@ -13,13 +13,22 @@ export default {
   },
   data(){
     return{
-
+      res:{}
     }
   },
   mounted(){
-    //storage.setItem('a',1);
-    //storage.setItem('user',{a:1});
-    //storage.setItem('abc',{a:1},'user');
+    // 本地加载请求静态json文件的形式
+    //this.axios.get('./mock/user/login.json').then((res) => {
+      //this.res = res;
+    //});
+    // 通过easy-mock平台实现数据mock
+    //this.axios.get('/user/login').then((res) => {
+      //this.res = res;
+    //});
+    // 本地集成mockjs实现数据mock
+    this.axios.get('/user/login').then((res) => {
+      this.res = res;
+    });
   }
 }
 </script>
