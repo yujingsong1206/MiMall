2e19f8575d8d83aae4a463af79dcaa5b5260fa11
yujingsong1206 <646783598@qq.com>
2020-03-08 09:41:07 +0800

    增加弹框模型

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -80,7 +80,7 @@
                   <div class="item-info">
                     <h3>{{item.name}}</h3>
                     <p>{{item.subtitle}}</p>
-                    <p class="price">{{item.price}}元</p>
+                    <p class="price" @click="addCart(item.id)">{{item.price}}元</p>
                   </div>
                 </div>
               </div>
@@ -89,18 +89,32 @@
         </div>
       </div>
     <service-bar></service-bar>
+    <modal title="提示" 
+           sureText="查看购物车"
+           btnType="1" 
+           modalType="middle" 
+           :showModal="showModal"
+           v-on:submit="goToCart"
+           v-on:cancel="showModal=false"
+           >
+           <template v-slot:body>
+             <p>商品添加成功！</p>
+           </template>
+    </modal>
   </div>
 </template>
 <script>
-  import ServiceBar from './../components/ServiceBar'
-  import { swiper, swiperSlide } from 'vue-awesome-swiper'
-  import 'swiper/dist/css/swiper.css'
+  import ServiceBar from './../components/ServiceBar';
+  import Modal from './../components/Modal';
+  import { swiper, swiperSlide } from 'vue-awesome-swiper';
+  import 'swiper/dist/css/swiper.css';
   export default{
     name:'index',
     components:{
       swiper,
       swiperSlide,
-      ServiceBar
+      ServiceBar,
+      Modal
     },
     data(){
       return {
@@ -183,7 +197,8 @@
                 img:'/imgs/ads/ads-4.jpg'
             }
         ],
-        phoneList:[]
+        phoneList:[],
+        showModal:false
       }
     },
     mounted(){
@@ -194,11 +209,26 @@
         this.axios.get('/products',{
           params:{
             categoryId:100012,
-            pageSize:8
+            pageSize:14
           }
         }).then((res)=>{
+          res.list = res.list.slice(6,14);
           this.phoneList = [res.list.slice(0,4),res.list.slice(4,8)];
         });
+      },
+      addCart(){
+        this.showModal = true;
+        // this.axios.post('/carts',{
+        //   productId:id,
+        //   selected:true
+        // }).then(()=>{
+
+        // }).catch(()=>{
+        //   this.showModal = true;
+        // })
+      },
+      goToCart(){
+        this.$router.push('/cart');
       }
     }
   }
