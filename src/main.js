f4f104109a9e004ee92dc5591ed9a398c3f58056
yujingsong1206 <646783598@qq.com>
2020-03-11 11:17:37 +0800

    商品详情页实现

--- src/main.js
+++ src/main.js
@@ -26,6 +26,7 @@ axios.interceptors.response.use(function(response){
     if(path != '#/index'){
       window.location.href = '/#/login';
     }
+    return Promise.reject(res);
   } else {
     alert(res.msg);
     return Promise.reject(res);
